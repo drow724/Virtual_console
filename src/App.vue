0dabf9d357232e0c0ba484e62fd95405505a1c8f
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2022-05-04 01:27:37 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="contatiner">
+    <MainComponent></MainComponent>
+  </div>
+</template>
+
+<script>
+import MainComponent from './components/MainComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  },
+  data() {
+    return {
+
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+  
+  },
+  methods: {
+
+  },
+  watch:{
+
+  }
+}
+</script>
+
+<style>
+body::-webkit-scrollbar{
+  display: none;
+}
+body {
+  overflow: hidden;
+  background-color : black;
+  font-family : 'consolab';
+  font-size: 13px;
+}
+@font-face {
+  font-family:'consola';
+  src: url('assets/fonts/consola.ttf') format('truetype');
+  font-weight: 400;
+}
+@font-face {
+  font-family:'consolab';
+  src: url('assets/fonts/consolab.ttf') format('truetype');
+  font-weight: 300;
+}
+@font-face {
+  font-family:'consolai';
+  src: url('assets/fonts/consolai.ttf') format('truetype');
+  font-weight: 700;
+}
+@font-face {
+  font-family:'consolaz';
+  src: url('assets/fonts/consolaz.ttf') format('truetype');
+  font-weight: 700;
+}
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <template :key="index" v-for="(log, index) in logs">
+    <LineComponent></LineComponent>
+  </template>
+</template>
+
+<script>
+import LineComponent from './LineComponent.vue';
+
+export default {
+  name: 'MainComponent',
+  components: {
+    LineComponent
+  },
+  data() {
+    return {
+      invisible : [],
+      logs : [],
+      isScroll : false
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+  },
+  mounted() {
+    setInterval(this.pushLine,1500);
+  },
+  unmounted() {
+  
+  },
+  methods: {
+    async pushLine() {
+      let line = LineComponent;
+      this.logs.push(line);
+    }
+  },
+  watch:{
+
+  },
+  updated() {
+    this.$nextTick(function () {
+      // if(window.innerHeight < document.body.clientHeight){
+      //   window.addEventListener('scroll', (event) => { 
+      //     document.body.style.overflow = 'scroll';
+      //     event.target;
+      //   });
+      // }
+      if(window.innerHeight < document.body.scrollHeight){
+        window.scrollTo(0,document.body.scrollHeight);
+      }
+
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: white;
+}
+</style>
